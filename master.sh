dfdb15050e1f8eff779dd69cb8463bcd057c75a0
Iyamoto <Iyamoto@users.noreply.github.com>
2013-09-24 10:43:19 -0700

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,8 +2,10 @@
 #INIT
 wget="/usr/local/bin/wget --no-check-certificate"
 base="/usr/local/domainer"
+gunzip="/usr/bin/gunzip -k"
 urls=$base"/urls.txt"
 
 #Get zone files
 cd $base/zone/
 $wget -i $urls -cN
+$gunzip *.gz
